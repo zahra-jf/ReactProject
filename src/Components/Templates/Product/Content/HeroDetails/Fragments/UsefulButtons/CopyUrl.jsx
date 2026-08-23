9cfd3bc489b81277efebba596d9827ff8dc55590
zahra-jf <z.jafari98b@gmail.com>
2026-08-23 20:30:35 +0330

    feat: share the product page

--- src/Components/Templates/Product/Content/HeroDetails/Fragments/UsefulButtons/CopyUrl.jsx
+++ src/Components/Templates/Product/Content/HeroDetails/Fragments/UsefulButtons/CopyUrl.jsx
@@ -1,10 +1,28 @@
 import { FaShare } from "react-icons/fa6";
 import Tooltip from "../../../../../../Ui/Tooltip";
+import { useState } from "react";
 
 const CopyUrl = () => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyUrl = () => {
+    const origin = location.origin;
+    const pathname = location.pathname;
+    const url = origin + pathname;
+
+    navigator.clipboard.writeText(url);
+
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000)
+
+  }
   return (
-    <Tooltip>
-      <button className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-slate-700">
+    <Tooltip text={isCopied ? "با موفقیت کپی شد" : "اشتراک گذاری"}>
+      <button className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-slate-700"
+        onClick={copyUrl}>
         <FaShare />
       </button>
     </Tooltip>
