2e6413271664dba0f0d72add722a6a011dbe2243
zahra-jf <z.jafari98b@gmail.com>
2026-08-24 15:45:37 +0330

    feat: scroll to coment section

--- src/Components/Templates/Product/Content/HeroDetails/Fragments/UsefulButtons/FocusOnComments.jsx
+++ src/Components/Templates/Product/Content/HeroDetails/Fragments/UsefulButtons/FocusOnComments.jsx
@@ -1,11 +1,27 @@
 import { FaComment } from "react-icons/fa6";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import Tooltip from "../../../../../../Ui/Tooltip";
+import { useEffect } from "react";
 
 const FocusOnComments = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const hash = location.hash;
+
+    if (hash) {
+      const element = document.querySelector(hash);
+      element.scrollIntoView({
+        behavior: "smooth"
+      }
+      )
+    }
+  }, [location])
   return (
     <Tooltip text={"نظرات کاربران"}>
-      <Link className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-sky-600">
+      <Link
+        to="#product-comments"
+        className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-sky-600">
         <FaComment />
       </Link>
     </Tooltip>
